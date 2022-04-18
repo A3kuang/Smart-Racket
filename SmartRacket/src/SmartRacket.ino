9aac219291d4c15d54c407bdb7a48ef7527821d1
A3kuang <103907762+A3kuang@users.noreply.github.com>
2022-04-17 17:17:43 -0700

    Initial Commit
    Starting Commit

---
+++ SmartRacket/src/SmartRacket.ino
@@ -0,0 +1,18 @@
+/*
+ * Project SmartRacket
+ * Description: Rackets that help children 
+ * Author: Andy Kuang
+ * Date: April 17 2022
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
